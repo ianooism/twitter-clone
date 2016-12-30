4620c2daa28bf48ed5b5edd5dabe69a78e1c46b0
ianooism <ianooism@gmail.com>
2016-12-30 18:50:08 +0800

    add-feed (#9)
    user can view own posts and posts from following

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: :home
   
   def home
-    render :home, locals: { posts: current_user.posts }
+    render :home, locals: { posts: feed_posts }
   end
+  
+  private
+    def feed_posts
+      current_user.following_posts + current_user.posts
+    end
 end
